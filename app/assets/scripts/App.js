d18ea65e3e358fa40d662220ed67d0dfe4f1edef
Gary <glwilsonii05@gmail.com>
2020-09-18 12:41:05 -0500

    added localhost and btn classes

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,2 +1,4 @@
 import '../styles/styles.css'
-
+if(module.hot){
+    module.hot.accept()
+}
--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,12 @@
          filename: 'bundled.js', 
          path: path.resolve(__dirname, 'app')
      }, 
+     devServer: {
+         contentBase:path.join(__dirname, 'app'),
+         hot: true,
+         port: 3000
+     },
      mode: 'development',
-     watch: true,
      module: {
          rules: [
              {
